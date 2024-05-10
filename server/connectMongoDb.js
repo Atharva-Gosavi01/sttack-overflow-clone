94a169404bded9089e148ec8444ffe892f4b120c
Atharva Gosavi <atharvagosavi34@gmail.com>
2024-05-10 15:44:53 +0530

    mongodb connected

--- server/connectMongoDb.js
+++ server/connectMongoDb.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  console.log('Attempting to connect to MongoDB at:', process.env.CONNECTION_URL);
   try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.CONNECTION_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error);
+    console.error('Connection error:', error);
     process.exit(1);
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
 import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
@@ -27,3 +28,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+// const DB_URL = process.env.CONNECTION_URL
+// mongoose.connect(DB_URL,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+//     console.log("MongoDB database connected")
+// }).catch((error)=>{
+//     console.log(error)
+// })

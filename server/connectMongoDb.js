02dbbdfd7fd81f1ea3be3f68a3ce9e69b11e742d
Atharva Gosavi <atharvagosavi34@gmail.com>
2024-05-29 16:34:55 +0530

    mongodb changes

--- server/connectMongoDb.js
+++ server/connectMongoDb.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   console.log('Attempting to connect to MongoDB at:', process.env.CONNECTION_URL);
   try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL);
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Connection error:', error);
